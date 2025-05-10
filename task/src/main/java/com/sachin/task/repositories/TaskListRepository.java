f0b61730605c99b8fe95cfd0678fb453eff044f1
sachin magar <gitsachin123@gmail.com>
2025-05-10 14:32:51 -0800

    TaskList Repository created

---
+++ task/src/main/java/com/sachin/task/repositories/TaskListRepository.java
@@ -0,0 +1,11 @@
+package com.sachin.task.repositories;
+
+import com.sachin.task.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+}
